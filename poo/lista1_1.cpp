3a072df9a52c4a6ec0e66c7a4f422312476a8730
PedroPaiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-03-21 14:28:55 -0300

    lista1_1.cpp

--- poo/lista1_1.cpp
+++ poo/lista1_1.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Agenda 
+{
+    private:
+    int numeroPrivado;
+    string nomePrivado;
+    
+    public:
+    
+    void definirNomePrivado(string nome) 
+    {
+        nomePrivado = nome;
+    }
+
+    void imprimirNomePrivado() 
+    {
+        cout << "O nome anotado na agenda é: " << nomePrivado<< ".";
+    }
+    
+    void definirNumeroPrivado(int numero) 
+    {
+        numeroPrivado = numero;
+    }
+
+    void imprimirNumeroPrivado() 
+    {
+        cout << "\nE o número de telefone é: " << numeroPrivado<<".";
+    }
+};
+
+int main() 
+{
+    Agenda myAgenda;
+
+    myAgenda.definirNomePrivado("Pedro");
+    
+    myAgenda.imprimirNomePrivado();;
+    
+    myAgenda.definirNumeroPrivado(123456789);
+
+    myAgenda.imprimirNumeroPrivado();
+
+    return 0;
+}
 
